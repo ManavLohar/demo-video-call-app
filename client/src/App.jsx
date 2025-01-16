f4167292e9a273f85872ff62af4bf017b3cb2a17
Faizan <faizanvohra382@gmail.com>
2025-01-16 12:43:13 +0530

    add the form in lobby

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Lobby from './components/Lobby'
 
 function App() {
   return (
     <>
-      
-    </>
+      <Lobby/>
+      </>
   )
 }
 
--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import {useSocket} from "../Context/Socket"
+import { useState,useEffect } from 'react'
 
 function Lobby() {
+
+  const socket = useSocket()
+  const [email,setEmail] =useState("")
+  const [room,setRoom] =useState("")
+
+  useEffect(() => {
+      socket.on('joined-room', );
+      return () => {
+        socket.off('joined-room', );
+      }
+    },[socket])
+
+  const handleJoinRoom = () => {
+    socket.emit("join-room",{emailId: email,room })
+    console.log("room joined")
+  }
+  
+
   return (
-    <div>Lobby</div>
+    <div>
+    <form >
+    
+    <label htmlFor="email">Email:</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      value={email}
+      onChange={e => setEmail(e.target.value)}
+     
+      required
+    />
+    <br /><br />
+
+   
+    <label htmlFor="roomId">Room ID:</label>
+    <input
+      type="text"
+      id="roomId"
+      name="roomId"
+      value={room}
+      onChange={e => setRoom(e.target.value)}
+      
+      required
+    />
+    <br /><br />
+
+    
+    <button onClick={handleJoinRoom} type="submit">Join</button>
+  </form>
+    </div>
   )
 }
 
