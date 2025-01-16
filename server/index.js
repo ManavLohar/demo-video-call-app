10aa0e86a6d348bd2a714321774b4907690b6a87
ManavLohar <manavlohar414141@gmail.com>
2025-01-16 15:32:33 +0530

    add user-joined emit

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ io.on("connection", (socket) => {
     const { email, room } = data;
     emailToSocketIdMap.set(email, socket.id);
     socketIdToEmailMap.set(socket.id, email);
+    io.to(room).emit("user-joined", { email, id: socket.id });
     socket.join(room);
     io.to(socket.id).emit("join-room", data);
   });
