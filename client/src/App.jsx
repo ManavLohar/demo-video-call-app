284e6fae777909c059a8db64cb4da6e642f43ede
Faizan <faizanvohra382@gmail.com>
2025-01-16 15:30:15 +0530

    Accept the emit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react";
 import "./App.css";
 import Lobby from "./components/Lobby";
+import Room from "./components/Room";
+import {Routes,Route} from 'react-router-dom'
+import { SocketProvider } from "./Context/Socket";
 
 function App() {
   return (
     <>
-      <Lobby />
+    <SocketProvider>
+    <Routes>
+    
+  
+    <Route path='/' element={<Lobby />} />
+    
+    <Route path='/room/:room' element={<Room/>} />
+    
+  </Routes>
+  </SocketProvider>
+      
     </>
   );
 }
--- client/src/components/Lobby.jsx
+++ client/src/components/Lobby.jsx
@@ -1,23 +1,33 @@
 import React, { useCallback } from "react";
 import { useState, useEffect } from "react";
 import { useSocket } from "../Context/Socket";
+import { useNavigate } from 'react-router-dom'
 
 function Lobby() {
   const [email, setEmail] = useState("");
   const [room, setRoom] = useState("");
   const socket = useSocket();
+  const navigate = useNavigate()
 
-  // useEffect(() => {
-  //     socket.on('joined-room', );
-  //     return () => {
-  //       socket.off('joined-room', );
-  //     }
-  //   },[socket])
+  
+    const handleJoinRoom=  useCallback(({room})=>{
+      navigate(`/room/${room}`)
+    },
+    [navigate]
+  )
 
-  const handleJoinRoom = useCallback(
+  useEffect(() => {
+      socket.on('join-room',handleJoinRoom );
+      return () => {
+        socket.off('join-room',handleJoinRoom );
+      }
+    },[socket])
+
+  const handleSubmit = useCallback(
     (e) => {
       e.preventDefault();
       socket.emit("join-room", { email, room });
+      //console.log("object");
     },
     [email, room, socket]
   );
@@ -49,7 +59,7 @@ function Lobby() {
         <br />
         <br />
 
-        <button onClick={handleJoinRoom} type="submit">
+        <button onClick={handleSubmit} type="submit">
           Join
         </button>
       </form>
